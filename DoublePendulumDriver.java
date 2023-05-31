ef7cd824d5eb0c4c9087ce2d2da114ef6c82365f
Sarannya Bhattacharya <83712724+Emperor963@users.noreply.github.com>
2023-05-31 01:52:57 -0500

    Update DoublePendulumDriver.java

--- DoublePendulumDriver.java
+++ DoublePendulumDriver.java
@@ -130,11 +130,11 @@ public class DoublePendulumDriver {
 		IntegralCalculator calculator = new IntegralCalculator(fn);
 
 		for(int i = 2; i < t.length; i++) {
-			//System.out.println("x");
+			
 			normalMode[i] = calculator.HarmonicFDM2(ndot0, n0, t[i], t[i-1], 100, w);
 			n0 = normalMode[i-1];
 			ndot0 = (normalMode[i-1] - normalMode[i-2]) / (t[i-1] - t[i-2]);
-			//System.out.println("x");
+			
 		}
 		
 		return normalMode;
